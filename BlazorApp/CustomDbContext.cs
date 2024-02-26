2fda54b3f2474279bb5f7d013034ce330db5a750
rafael <rafaondjango@gmail.com>
2024-02-26 19:10:36 -0300

    feat: configurando acesso ao banco de dados e migrações iniciais da model User

---
+++ BlazorApp/CustomDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorApp;
+
+public class CustomDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; }
+
+	public CustomDbContext()
+	{
+
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		string connectionString = "User ID=root;Password=rafa123;Host=localhost;Port=5432;Database=entitystudy;";
+
+		optionsBuilder
+			.UseNpgsql(connectionString);
+	}
+
+}
--- BlazorApp/User.cs
+++ BlazorApp/User.cs
@@ -2,6 +2,7 @@
 
 public class User(string name, int age)
 {
+    public int Id { get; set; }
     public string Name { get; set; } = name;
     public int Age { get; set; } = age;
 }
