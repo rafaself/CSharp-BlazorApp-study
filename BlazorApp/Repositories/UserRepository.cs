d632b13ec3a73609f929a342cbdc087164a24250
rafael <rafaondjango@gmail.com>
2024-02-26 19:18:20 -0300

    feat: list e create user

---
+++ BlazorApp/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Repositories;
+
+public class UserRepository(DbContext dbContext)
+{
+	DbContext _dbContext = dbContext;
+
+	public void Add(User user)
+	{
+		_dbContext.Add(user);
+	}
+
+	public async Task<List<User>> List()
+	{
+		return await _dbContext
+			.Set<User>()
+			.AsNoTracking()
+			.OrderBy(user => user.Id)
+			.ToListAsync();
+	}
+
+}
