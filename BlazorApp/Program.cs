c4b6a80927a5b507ad26ad044f861fb4b05e270e
rafael <rafaondjango@gmail.com>
2024-02-26 21:27:02 -0300

    feat: implementando injeção com blazor

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,10 +1,13 @@
 using BlazorApp.Components;
+using BlazorApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<UserRepository>();
 
 var app = builder.Build();
 
